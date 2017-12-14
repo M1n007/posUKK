973770699314417540680f8d9dac73114687cff2
M1n007 <amin4udin@gmail.com>
2017-12-14 14:13:16 +0700

    Cacad v1

--- about.php
+++ about.php
@@ -1,4 +1,27 @@
-<?php require 'configuration/konek.php'; ?>
-<center>Project APP_KAsir yang tertunda :) Lolz..
-<br>
-Masih dalam tahap pembuatan :D</center>
+
+<?php require_once 'layout/header.php' ?>
+
+<div class="container">
+
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<font class="modal-title">Login Form</font>
+					</div>
+					<div class="modal-body">
+						<form action="" method="">
+							<label>Username :</label>
+							<input class="form-control" type="text" name="userlog" placeholder="masukan username...">
+							<label>Password :</label>
+							<input class="form-control" type="text" name="userlog" placeholder="masukan password...">
+						</form>
+					</div>
+					<div class="modal-footer">
+						<button class="btn btn-primary">Login</button>
+					</div>
+				</div>
+			</div>
+			
+</div>
+
+<?php require_once 'layout/footer.php' ?>
---
+++ layout/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pos APP_KASIR</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script type="text/javascript" href="js/bootstrap.js"></script>
+	<script type="text/javascript" href="js/jquery.js"></script>
+</head>
+<body>
